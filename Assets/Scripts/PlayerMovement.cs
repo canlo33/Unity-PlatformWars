1104ef92d74df27b271f22502e2ed65492c2add6
Can Lo <clo51@my.centennialcollege.ca>
2019-01-08 17:56:45 -0500

    Jump + Charged Jump
    Jump + Charged Jump has been added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool onGround;
+    private Rigidbody rigidBody;
+    private float jumpPressure;
+    public float minJump;
+    public float maxJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        onGround = true;
+        jumpPressure = 0f;
+        minJump = 5f;
+        maxJump = 10f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // ! Jump {
+        // Charged Jump 
+        if(onGround)
+        {
+            if(Input.GetButton("Jump"))
+            {
+                if(jumpPressure < maxJump)
+                {
+                    jumpPressure += Time.deltaTime * 6f;
+                }
+                else
+                {
+                    jumpPressure = maxJump;
+                }
+               
+                
+            }
+
+            // Regular Jump
+
+        else
+            {
+                if(jumpPressure > 0f)
+                {
+                    jumpPressure += minJump;
+                    Jump();
+                }
+                
+            }
+        }
+
+        // ! Jump }
+    }
+
+
+    private void OnCollisionEnter(Collision collider)
+    {
+       if(collider.gameObject.CompareTag("ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.velocity = new Vector3(0f, jumpPressure, 0f);
+        jumpPressure = 0f;
+        onGround = false;
+    }
+  
+
+
+}
